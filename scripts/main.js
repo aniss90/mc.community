107dbd563cc84aa4238f1073a8473230e50c6eb6
aniss90 <anisluonadi99@gmail.com>
2025-05-09 22:35:31 +0100

    New additions

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,8 @@
-// تأكد من استيراد Firebase بشكل صحيح
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
 import { getFirestore, collection, addDoc, onSnapshot, doc, updateDoc, setDoc, increment } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-storage.js"; // جديد
 
-// إعداد Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyBf-HKpI4G5p6E0xP1OHGLxtDTGwE_0gg0",
   authDomain: "minecraft-community-4fd3d.firebaseapp.com",
@@ -14,12 +13,11 @@ const firebaseConfig = {
   measurementId: "G-F18SFG2LMZ"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
+const storage = getStorage(app); // جديد
 
-// تسجيل الزوار
 const visitorRef = doc(db, "stats", "visitors");
 updateDoc(visitorRef, {
   count: increment(1)
@@ -27,22 +25,19 @@ updateDoc(visitorRef, {
   await setDoc(visitorRef, { count: 1 });
 });
 
-// دالة لفتح مواقع ويب
 export function openWebsite(url) {
   window.open(url, "_blank");
 }
 
-// دالة صوت النقر
 export function onClick() {
   var audio = new Audio('sounds/button_click.ogg');
   audio.play();
 }
 
-// دالة لعرض التنبيهات
 export function showAlert(message, type) {
   const alertBox = document.createElement('div');
   alertBox.classList.add('custom-alert');
-  alertBox.classList.add(type); // إما success أو error
+  alertBox.classList.add(type);
   alertBox.innerHTML = `${message}<br><button onclick="closeAlert(this)">موافق</button>`;
   document.body.appendChild(alertBox);
   
@@ -54,12 +49,10 @@ export function showAlert(message, type) {
   alertBox.style.display = 'block';
 }
 
-// دالة لإغلاق التنبيه
 export function closeAlert(button) {
   const alertBox = button.parentElement;
   alertBox.style.display = 'none';
   document.body.removeChild(alertBox);
 }
 
-// تصدير الدوال والكائنات اللازمة
-export { db, auth, collection, addDoc, onSnapshot, doc, updateDoc, setDoc };
+export { db, auth, storage, collection, addDoc, onSnapshot, doc, updateDoc, setDoc };
