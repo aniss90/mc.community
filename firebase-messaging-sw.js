446e22dbcd30bbe64ffa47fae309d80800db7561
aniss90 <anisluonadi99@gmail.com>
2025-05-13 20:48:09 +0100

    update

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,25 +1,14 @@
-importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js");
+importScripts('https://www.gstatic.com/firebasejs/9.22.2/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.22.2/firebase-messaging-compat.js');
 
 firebase.initializeApp({
   apiKey: "AIzaSyBf-HKpI4G5p6E0xP1OHGLxtDTGwE_0gg0",
   authDomain: "minecraft-community-4fd3d.firebaseapp.com",
   projectId: "minecraft-community-4fd3d",
-  storageBucket: "minecraft-community-4fd3d.firebasestorage.app",
+  storageBucket: "minecraft-community-4fd3d.appspot.com",
   messagingSenderId: "550140131027",
   appId: "1:550140131027:web:a5186726cf5372e138c626",
   measurementId: "G-F18SFG2LMZ"
 });
 
 const messaging = firebase.messaging();
-
-messaging.onBackgroundMessage((payload) => {
-  console.log("Received background message ", payload);
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-    icon: "/icon.png"
-  };
-
-  self.registration.showNotification(notificationTitle, notificationOptions);
-});
