2b62e1ee8fd09b113d80ec106dfd8e7e0d1ce083
aniss90 <anisluonadi99@gmail.com>
2025-08-09 16:13:08 +0100

    update

---
+++ scripts/tags.js
@@ -0,0 +1,19 @@
+// js/tags.js
+export const tagNames = {
+  'admin': 'ADMIN',
+  'moderator': 'MOD',
+  'vip': 'VIP',
+  'builder': 'BUILDER',
+  'friend': 'FRIEND',
+  'legend': 'LEGEND',
+  'youtuber': 'YOUTUBER',
+  'mvp': 'MVP',
+};
+
+export function getTagName(tagId) {
+  return tagNames[tagId] || tagId;
+}
+
+export function getAllTagIds() {
+  return Object.keys(tagNames);
+}
